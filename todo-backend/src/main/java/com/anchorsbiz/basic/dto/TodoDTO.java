45f5ea3ceb86bfac025f95ae8804d37f0bd0cccc
모함메드 <103490565+mohamedashour89@users.noreply.github.com>
2025-03-05 18:08:24 +0900

    changes

--- todo-backend/src/main/java/com/anchorsbiz/basic/dto/TodoDTO.java
+++ todo-backend/src/main/java/com/anchorsbiz/basic/dto/TodoDTO.java
@@ -1,8 +1,9 @@
 package com.anchorsbiz.basic.dto;
 
+import lombok.Data;
 import java.time.LocalDate;
 
-
+@Data
 public class TodoDTO {
     private String title;
     private String description;
@@ -10,29 +11,5 @@ public class TodoDTO {
     private String assignee;
     private LocalDate targetDate;
     private String status;
-
-    // Getter and settters
-    public String getTitle() { return title;}
-    public void setTitle(String title) {this.title = title;}
-
-
-    public String getDescription() { return description;}
-    public void setDescription(String description) {this.description = description;}
-
-    public String getAssigner() { return assigner;}
-    public void setAssigner (String assigner){this.assigner = assigner;}
-
-    public String getAssignee() { return assignee;}
-    public void setAssignee (String assignee){this.assignee = assignee;}
-
-    public LocalDate getTargetDate() { return targetDate;}
-    public void setTargetDate(LocalDate targetDate){this.targetDate = targetDate;}
-
-    public String getStatus() { return status;}
-    public void setStatus (String status) {this.status = status;}
-
-
-
-
 }
 
--- todo-backend/src/main/java/com/anchorsbiz/basic/entity/Todo.java
+++ todo-backend/src/main/java/com/anchorsbiz/basic/entity/Todo.java
@@ -6,8 +6,7 @@ import lombok.Setter;
 import jakarta.persistence.*;
 import java.time.LocalDate;
 
-@Getter
-@Setter
+
 @Data
 @Entity
 @Table(name = "todo")
@@ -23,55 +22,4 @@ public class Todo {
         @Column(name = "target_date")
         private LocalDate targetDate;
         private String status;
-
-
-        // getters and setters 
-        public Long getId(){
-            return id;
-        }
-        public void setId(Long id){
-            this.id = id;
-        }
-
-        public String getTitle(){
-            return title;
-        }
-        public void setTitle(String title){
-            this.title = title;
-        }
-
-        public String getDescription(){
-            return description;
-        }
-        public void setDescription(String description){
-            this.description = description;
-        }
-        public String getAssigner(){
-            return assigner;
-        }
-        public void setAssigner(String assigner){
-            this.assigner = assigner;
-        }
-        public String getAssignee(){
-            return assignee;
-        }
-        public void setAssignee(String assignee){
-            this.assignee = assignee;
-        }
-
-        public LocalDate getTargetDate(){
-            return targetDate;
-        }
-        public void setTargetDate(LocalDate targetDate){
-            this.targetDate = targetDate;
-        }
-
-        public String getStatus(){
-            return status;
-        }
-
-        public void setStatus(String status){
-            this.status = status;
-        }
-
 }
